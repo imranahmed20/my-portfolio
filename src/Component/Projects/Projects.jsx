3bc0b8d9755e818d5a6823f99d421a6444972e28
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-07-01 07:33:21 +0600

    a tag and title added

--- src/Component/Projects/Projects.jsx
+++ src/Component/Projects/Projects.jsx
@@ -12,17 +12,24 @@ const Projects = () => {
                     <div className="card">
                         <img src={project1} className="card-img-top p-2" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-
+                            <h4 className="card-title">Chef Recipe</h4>
+                            <div className='d-flex justify-content-between align-items-center mt-3'>
+                                <a href="https://github.com/imranahmed20/chef-recipe-client" title='Github Client'>a</a>
+                                <a href="https://github.com/imranahmed20/chef-recipe-server">a</a>
+                                <a href="https://chef-recipe-client-15e57.web.app">a</a>
+                            </div>
                         </div>
+
                     </div>
                 </div>
                 <div className="col">
                     <div className="card">
                         <img src={project2} className="card-img-top p-2" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-
+                            <h4 className="card-title">Sports Academies</h4>
+                        </div>
+                        <div class="card-footer">
+                            <small class="">Last updated 3 mins ago</small>
                         </div>
                     </div>
                 </div>
@@ -30,8 +37,10 @@ const Projects = () => {
                     <div className="card">
                         <img src={project3} className="card-img-top p-2" alt="..." />
                         <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-
+                            <h4 className="card-title">Doll Marketplace</h4>
+                        </div>
+                        <div class="card-footer">
+                            <small class="">Last updated 3 mins ago</small>
                         </div>
                     </div>
                 </div>
