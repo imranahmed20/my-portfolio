8c67842b337b5bd9008f5dcbddcc6fac6ebb2850
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-07-03 18:18:59 +0600

    recover all size

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
             <div className='banner container mb-5' id="about">
                 <h1 className='text-center fw-bold mb-5'>About Me</h1>
                 <div className='row row-cols-1 row-cols-md-2 g-4'>
-                    <div className='col' data-aos="fade-right">
+                    <div className='col col-md-5' data-aos="fade-right">
                         <h4 className='fw-bold mb-4'>Md Imran Ahmed</h4>
                         <p className='fs-5 fw-semibold'>I am a MERN Stack Web Developer</p>
                         <h5 className='mb-4'>Age: 22 years</h5>
@@ -16,7 +16,7 @@ const About = () => {
                         <h5 className='mb-4'>Language: English, Bengali</h5>
                         <h5 className='mb-4'>Address: Haragach, Rangpur, Bangladesh</h5>
                     </div>
-                    <div className="col" data-aos="fade-left"
+                    <div className="col col-md-7" data-aos="fade-left"
                         data-aos-duration="1500">
                         <p className='fs-5 fw-semibold'>Hello, I'm Imran Ahmed, a passionate and dedicated frontend web developer. I am a committed student in my Third year of studies at Kurigram Govt Collage, Kurigram. My journey into the world of programming began when I discovered my love for creating visually appealing and interactive websites.To further enhance my skills, I enrolled in the "Programming Hero" Course, where I completed the web development curriculum and obtained a certificate. This course provided me with comprehensive knowledge and practical experience, enabling me to tackle real-world projects with confidence. When I'm not coding, you can find me indulging in my hobbies.I'm also a big fan of anime series and appreciate the artistry and storytelling in this medium.
                             <br />
