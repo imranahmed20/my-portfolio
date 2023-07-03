d1a86cffc83ae192b79ba8b92b80bab2ad5484e9
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-07-03 18:05:20 +0600

    updata letter

--- src/Component/About/About.jsx
+++ src/Component/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
                 <h1 className='text-center fw-bold mb-5'>About Me</h1>
                 <div className='row row-cols-1 row-cols-md-2 g-4'>
                     <div className='col' data-aos="fade-right">
-                        <h4 className='fw-bold mb-4'><span>Name:</span>  Md.Imran Ahmed</h4>
+                        <h4 className='fw-bold mb-4'>Md Imran Ahmed</h4>
                         <p className='fs-5 fw-semibold'>I am a MERN Stack Web Developer</p>
                         <h5 className='mb-4'>Age: 22 years</h5>
                         <h5 className='mb-4'>Phone: 01300193867</h5>
@@ -18,9 +18,9 @@ const About = () => {
                     </div>
                     <div className="col" data-aos="fade-left"
                         data-aos-duration="1500">
-                        <h5 className=' fw-semibold'>Hello, I'm Imran Ahmed, a passionate and dedicated frontend web developer. I am a committed student in my Third year of studies at Kurigram Govt Collage, Kurigram. My journey into the world of programming began when I discovered my love for creating visually appealing and interactive websites.To further enhance my skills, I enrolled in the "Programming Hero" Course, where I completed the web development curriculum and obtained a certificate. This course provided me with comprehensive knowledge and practical experience, enabling me to tackle real-world projects with confidence. When I'm not coding, you can find me indulging in my hobbies.I'm also a big fan of anime series and appreciate the artistry and storytelling in this medium.
+                        <p className='fs-5 fw-semibold'>Hello, I'm Imran Ahmed, a passionate and dedicated frontend web developer. I am a committed student in my Third year of studies at Kurigram Govt Collage, Kurigram. My journey into the world of programming began when I discovered my love for creating visually appealing and interactive websites.To further enhance my skills, I enrolled in the "Programming Hero" Course, where I completed the web development curriculum and obtained a certificate. This course provided me with comprehensive knowledge and practical experience, enabling me to tackle real-world projects with confidence. When I'm not coding, you can find me indulging in my hobbies.I'm also a big fan of anime series and appreciate the artistry and storytelling in this medium.
                             <br />
-                            I am excited to embark on new projects and collaborate with like-minded individuals. Feel free to browse through my portfolio and get in touch with me if you have any inquiries or if you'd like to discuss potential opportunities.</h5>
+                            I am excited to embark on new projects and collaborate with like-minded individuals. Feel free to browse through my portfolio and get in touch with me if you have any inquiries or if you'd like to discuss potential opportunities.</p>
                     </div>
 
                 </div>
--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -4,14 +4,14 @@ import img from '../../../src/assets/Image/357788537_2237658816426893_3189938461
 import resume from '../../assets/Image/Imran Ahmed Resume.pdf'
 import { FaDownload } from 'react-icons/fa';
 
+
 const Banner = () => {
     return (
         <div className='d-md-flex justify-content-between align-items-center mt-5 container' >
             <div>
                 <h1>Hi! I'm <br /><span className='text-success fw-bold'>Imran Ahmed</span></h1>
-
                 <h1 className='text-danger fw-bold fs-1'>Full Stack Web Developer </h1>
-                <p className='fs-5 fw-semibold'>Welcome to my portfolio! I'm a passionate MERN stack web developer with expertise in HTML, CSS, JavaScript, React, Express.js, Node.js, and MongoDB. With a strong foundation in front-end development, I strive to create captivating user interfaces and seamless user experiences. I enjoy turning ideas into functional and visually appealing websites and web applications.</p>
+                <p className='fs-5 fw-semibold mb-3'>Welcome to my portfolio! I'm a passionate MERN stack web developer with expertise in HTML, CSS, JavaScript, React, Express.js, Node.js, and MongoDB. With a strong foundation in front-end development, I strive to create captivating user interfaces and seamless user experiences. I enjoy turning ideas into functional and visually appealing websites and web applications.</p>
                 <a href={resume} target="_blank" download="Imran Ahmed Resume" rel="noreferrer"> <button className='btn btn-danger fw-semibold fs-5'>DownLoad Resume <FaDownload></FaDownload></button></a>
             </div>
             <div className='ms-5 mt-3'>
